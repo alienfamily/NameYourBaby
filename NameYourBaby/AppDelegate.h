ad8f081f78612160c9f03981237c30b606fae692
alienfamily <alexandre.remy.d@gmail.com>
2012-08-27 18:46:05 +0200

    Fixing core data bullshit

--- NameYourBaby/AppDelegate.h
+++ NameYourBaby/AppDelegate.h
@@ -16,4 +16,12 @@
 
 @property (strong, nonatomic) ViewController *viewController;
 
+// Core data things
+@property (nonatomic, strong, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, strong, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, strong, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
 @end
