be54a971f243eb69fae7925f32ada7455f48d1b7
alienfamily <alexandre.remy.d@gmail.com>
2012-08-27 18:04:49 +0200

    Setting up core data model

---
+++ NameYourBaby/Babies.h
@@ -0,0 +1,19 @@
+//
+//  Babies.h
+//  NameYourBaby
+//
+//  Created by Bou on 27/08/12.
+//  Copyright (c) 2012 Bou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Babies : NSManagedObject
+
+@property (nonatomic, retain) NSString * name;
+@property (nonatomic, retain) NSNumber * type;
+@property (nonatomic, retain) NSNumber * fav;
+
+@end
---
+++ NameYourBaby/Babies.m
@@ -0,0 +1,18 @@
+//
+//  Babies.m
+//  NameYourBaby
+//
+//  Created by Bou on 27/08/12.
+//  Copyright (c) 2012 Bou. All rights reserved.
+//
+
+#import "Babies.h"
+
+
+@implementation Babies
+
+@dynamic name;
+@dynamic type;
+@dynamic fav;
+
+@end
